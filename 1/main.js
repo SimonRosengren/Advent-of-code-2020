59e0ea7544228975898c7909340100c5c29e24e9
simonrosengren <simonrosengren123@gmail.com>
2020-12-14 20:35:29 +0100

    second

--- 1/main.js
+++ 1/main.js
@@ -1,10 +1,31 @@
-var fs = require('fs');ß
+var fs = require('fs');
 const input = fs.readFileSync('input.txt', 'utf8');
 const data = input.toString().split('\n')
 
+// Part I
 data.forEach(val => {
     let valueToFind = 2020 - val;
     if(data.includes(valueToFind.toString())) {
         console.log(valueToFind * val);
     }
 })
+
+// Part II
+data.forEach(val => {
+    let rest = 2020 - val;
+    data.forEach(val => {
+        
+    })
+})
+
+
+
+
+const findOtherHalf = (part, total) => {
+    data.forEach(val => {
+        let valueToFind = total - val;
+        if(data.includes(valueToFind.toString())) {
+            return valueToFind
+        }
+    })
+}
