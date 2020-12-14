c7357dd47fc783b1947af13d9efa317509536d1b
simonrosengren <simonrosengren123@gmail.com>
2020-12-14 22:31:34 +0100

    two part two

--- 2/main.js
+++ 2/main.js
@@ -2,6 +2,8 @@ var fs = require('fs')
 const fullInput = fs.readFileSync('./input.txt', 'utf8')
 
 let nrOfCorrectPasswords = 0
+let nrOfCorrectPasswordsPartTwo = 0;
+// Part I
 
 fullInput.toString().split('\n').forEach(input => {
     const splitData = input.toString().split(':')
@@ -14,11 +16,29 @@ fullInput.toString().split('\n').forEach(input => {
     const maxAmount = splitPolicy[1]
     const letter = splitPolicy[2]
 
+    // Part I
     const nrOfOccurences = (password.match(new RegExp(letter, 'g')) || {}).length
 
     if (nrOfOccurences >= minAmount && nrOfOccurences <= maxAmount) {
         nrOfCorrectPasswords++
     }
+
+    // Part II
+
+    if ( password[parseInt(minAmount) + 1] === letter ) {
+        if ( password[parseInt(maxAmount) + 1] !== letter ) {
+            nrOfCorrectPasswordsPartTwo++;
+        }
+    }
+
+    if ( password[parseInt(maxAmount) + 1] === letter ) {
+        if ( password[parseInt(minAmount) + 1] !== letter ) {
+            nrOfCorrectPasswordsPartTwo++;
+        }
+    }
 })
 
-console.log(nrOfCorrectPasswords)
+console.log(`Part I: ${nrOfCorrectPasswords}`)
+
+console.log(`Part II: ${nrOfCorrectPasswordsPartTwo}`)
+
